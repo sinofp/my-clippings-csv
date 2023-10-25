e56e516f435bd6b24c52bc82e4726cb3475867bd
sinofp <sinofp@tuta.io>
2023-10-25 23:42:43 +0100

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake for my-clippings-csv";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs.haskellPackages; [
+            cabal-install
+            (ghcWithPackages (ps: with ps; [
+              megaparsec
+              cassava
+              hspec
+              hspec-megaparsec
+              haskell-language-server
+            ]))
+          ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.haskellPackages.developPackage {
+          name = "my-clippings-csv";
+          root = ./.;
+        };
+      });
+    };
+}
